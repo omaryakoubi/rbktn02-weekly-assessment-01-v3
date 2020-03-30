14cf49fa54f6b673cc733d3973e8156d2b3d028b
omaryakoubi <yakoubiomar2001@gmail.com>
2020-03-30 11:42:54 +0100

    Assessment 1V3

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -1,23 +1,23 @@
 /**
-  *
-  * Implement a `map` method on this Tree class, using pseudoclassical instantiation.
-  *
-  * Map accepts a mapping function as its only argument. It traverses the tree,
-  * passing each node's value into the mapping function, and generates a new
-  * tree containing the results.
-  *
-  * So `map` should return a tree with the same structure, and different values,
-  * but it should NOT modify the tree that was passed in.
-  *
-  * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   var newTree = root1.map(function (value) {
+*
+* Implement a `map` method on this Tree class, using pseudoclassical instantiation.
+*
+* Map accepts a mapping function as its only argument. It traverses the tree,
+* passing each node's value into the mapping function, and generates a new
+* tree containing the results.
+*
+* So `map` should return a tree with the same structure, and different values,
+* but it should NOT modify the tree that was passed in.
+*
+* Example:
+*   var root1 = new Tree(1);
+*   var branch2 = root1.addChild(2);
+*   var branch3 = root1.addChild(3);
+*   var leaf4 = branch2.addChild(4);
+*   var leaf5 = branch2.addChild(5);
+*   var leaf6 = branch3.addChild(6);
+*   var leaf7 = branch3.addChild(7);
+*   var newTree = root1.map(function (value) {
   *     return value * 2;
   *   })
   *  newTree.value // 2
@@ -27,11 +27,48 @@
   *  newTree.children[1].children[1].value // 14
   *  root1.value // still 1
   */
-
-var Tree = function(value) {
-  this.value = value;
-  this.children = [];
-};
-
-
-
+  
+  var Tree = function(value) {
+    this.value = value;
+    this.children = [];
+  };
+  
+  Tree.prototype.map = function (callback) {
+    var newTree = new Tree(callback(this.value));
+    for (var i = 0; i < this.children.length; i++) {
+      newTree.children.push(this.children[i].map(callback));
+    }
+    return newTree;
+  };
+  
+  
+  Tree.prototype.addChild = function(child) {
+    if (!child || !(child instanceof Tree)) {
+      child = new Tree(child);
+    }
+    
+    if (!this.child) {
+      this.children.push(child);
+    } else {
+      ('This child is already a child of the tree');
+    }
+    
+    return child;
+  };
+  
+  
+  
+  Tree.prototype.removeChild = function(child) {
+    var index = this.children.indexOf(child);
+    if (index !== -1) {
+      
+      this.children.splice(index, 1);
+    } else {
+      ('this node is not direct son child of this tree');
+    }
+  };
+  
+  
+  
+  
+  
